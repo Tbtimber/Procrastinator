52a3eda85f88ebd2f46a5d91d58b6f51fa13817e
TAvelis <avelitj12@juniata.edu>
2016-01-21 10:56:59 +0100

    Add async skeleton

---
+++ app/src/main/java/com/procrastinator/isen/procrastinator/imdbRetrieval/GetSearchResultsAsync.java
@@ -0,0 +1,13 @@
+package com.procrastinator.isen.procrastinator.imdbRetrieval;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Tristan on 1/21/2016.
+ */
+public class GetSearchResultsAsync extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] params) {
+        return null;
+    }
+}
