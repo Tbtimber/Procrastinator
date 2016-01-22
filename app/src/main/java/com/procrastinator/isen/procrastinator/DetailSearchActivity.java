822fb54d6083ad4ccb3c8485c46c360d1c7a28fe
Tbtimber <delpierre.matthieu@gmail.com>
2016-01-22 09:44:52 +0100

    Added search bar in header (looks gross !)

--- app/src/main/java/com/procrastinator/isen/procrastinator/DetailSearchActivity.java
+++ app/src/main/java/com/procrastinator/isen/procrastinator/DetailSearchActivity.java
@@ -1,10 +1,16 @@
 package com.procrastinator.isen.procrastinator;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 
 /**
  * Created by Matthieu on 14/01/2016.
  */
 public class DetailSearchActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_search_activity);
+    }
 }
--- app/src/main/java/com/procrastinator/isen/procrastinator/MainActivity.java
+++ app/src/main/java/com/procrastinator/isen/procrastinator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.procrastinator.isen.procrastinator;
 
+import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
@@ -9,9 +10,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -39,15 +42,17 @@ public class MainActivity extends AppCompatActivity implements MainActivityListe
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        android.support.v7.app.ActionBar abar =  getSupportActionBar();
+        abar.setDisplayShowTitleEnabled(false);
+        abar.setDisplayUseLogoEnabled(false);
+        abar.setDisplayHomeAsUpEnabled(false);
+        abar.setDisplayShowCustomEnabled(true);
+        abar.setDisplayShowHomeEnabled(false);
 
+        ActionBar.LayoutParams lp1 = new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        View customNav = LayoutInflater.from(this).inflate(R.layout.search_test, null);
+
+        abar.setCustomView(customNav);
 
         selectionAdaptedFrag = new SelectionFragment();
         selectionTrendingFrag = new SelectionFragmentTrending();
